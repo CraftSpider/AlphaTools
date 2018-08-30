0cb21ba8a2a1bdfba4232b90b268f220c16cbb3f
Rune Tynan <runetynan@gmail.com>
2018-08-30 16:16:10 -0400

    Begin work on generic formatting function

--- tests/test_logging.cpp
+++ tests/test_logging.cpp
@@ -8,7 +8,7 @@ using namespace logging;
 
 void TestLogger::before_class() {
     old_cout = std::cout.rdbuf();
-    std::ostringstream new_out;
+    new_out = std::ostringstream();
     std::cout.rdbuf(new_out.rdbuf());
 }
 
@@ -21,6 +21,12 @@ void TestLogger::run() {
 void TestLogger::after_class() {
     std::cout.rdbuf(old_cout);
 }
+
+void TestLogger::test_root() {}
+
+void TestLogger::test_normal() {}
+
+void TestLogger::test_format() {}
 
 void run_logging_tests() {
     TEST_CLASS(TestLogger)
