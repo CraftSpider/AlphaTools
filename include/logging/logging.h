2111b13fb5b333db1510b65fb9ff978411dcfe85
CraftSpider <runetynan@gmail.com>
2019-02-06 22:48:56 -0500

    Revert logger change, it doesn't help

--- include/logging/logging.h
+++ include/logging/logging.h
@@ -11,6 +11,6 @@ void set_default_level(const Level& level);
 void add_default_handler(Handler *handler);
 bool remove_default_handler(Handler *handler);
 Logger* get_root_logger();
-Logger* get_logger(std::string name, bool auto_parent = true);
+Logger* get_logger(const std::string& name, bool auto_parent = true);
 
 }
