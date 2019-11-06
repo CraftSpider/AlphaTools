bd72e8fcd93d5b89a8dc0433f38ec9754961cec1
Rune Tynan <runetynan@gmail.com>
2019-11-05 22:47:00 -0500

    Add TypeFinder tests

--- include/utils/sfinae.h
+++ include/utils/sfinae.h
@@ -7,29 +7,29 @@ namespace util {
 template<typename T>
 class TypeFinder {
 
-    typedef char _class[1];
-    typedef char _function[2];
-    typedef char _method[3];
-    typedef char _pointer[4];
+    typedef const char _class[1];
+    typedef const char _function[2];
+    typedef const char _method[3];
+    typedef const char _pointer[4];
 
     template<typename C, std::enable_if_t<std::is_class<C>::value, int> = 0>
-    static _class& test();
+    static _class& test() {return "";}
 
     template<typename C, std::enable_if_t<std::is_member_function_pointer<C>::value, int> = 0>
-    static _method& test();
+    static _method& test() {return "aa";}
 
-    template<typename C, std::enable_if_t<std::is_pointer<C>::value && std::is_function<std::remove_pointer<C>::type>::value, int> = 0>
-    static _function& test();
+    template<typename C, std::enable_if_t<std::is_function<C>::value || (std::is_pointer<C>::value && std::is_function<typename std::remove_pointer<C>::type>::value), int> = 0>
+    static _function& test() {return "a";}
 
-    template<typename C, std::enable_if_t<std::is_pointer<C>::vale && !std::is_function<std::remove_pointer<C>::type>::value, int> = 0>
-    static _pointer& test();
+    template<typename C, std::enable_if_t<std::is_pointer<C>::value && !std::is_function<typename std::remove_pointer<C>::type>::value, int> = 0>
+    static _pointer& test() {return "aaa";}
 
 public:
     enum {
-        cls = sizeof(test()) == sizeof(_class),
-        function = sizeof(test()) == sizeof(_function),
-        method = sizeof(test()) == sizeof(_method),
-        pointer = sizeof(test()) == sizeof(_pointer),
+        cls = sizeof(test<T>()) == sizeof(_class),
+        function = sizeof(test<T>()) == sizeof(_function),
+        method = sizeof(test<T>()) == sizeof(_method),
+        pointer = sizeof(test<T>()) == sizeof(_pointer),
         other = !cls && !function && !method && !pointer
     };
 };
--- tests/test_sfinae.cpp
+++ tests/test_sfinae.cpp
@@ -3,6 +3,32 @@
 #include "utils/sfinae.h"
 #include "test_sfinae.h"
 
+void A() {}
+
+struct B {
+    void C() {}
+};
+
+void test_typefinder() {
+    typedef util::TypeFinder<decltype(&A)> FuncFinder;
+    typedef util::TypeFinder<B> ClassFinder;
+    typedef util::TypeFinder<decltype(&B::C)> MethodFinder;
+    typedef util::TypeFinder<B*> PtrFinder;
+
+    ASSERT(FuncFinder::function);
+    ASSERT(!FuncFinder::cls && !FuncFinder::method && !FuncFinder::pointer);
+
+    ASSERT(ClassFinder::cls);
+    ASSERT(!ClassFinder::function && !ClassFinder::method && !ClassFinder::pointer);
+
+    ASSERT(MethodFinder::method);
+    ASSERT(!MethodFinder::cls && !MethodFinder::function && !MethodFinder::pointer);
+
+    ASSERT(PtrFinder::pointer);
+    ASSERT(!PtrFinder::cls && !PtrFinder::method && !PtrFinder::function);
+
+}
+
 void test_makeptr() {
     typedef util::MakePtr<int>::type Type1;
     typedef util::MakePtr<int*>::type Type2;
@@ -15,5 +41,6 @@ void test_makeptr() {
 }
 
 void run_sfinae_tests() {
+    TEST(test_typefinder)
     TEST(test_makeptr)
 }
